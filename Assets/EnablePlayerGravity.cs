15f80e0259408bc4f6ff10eddc256385a97cdefe
Aitmyrza Kubanychbekov <aitmyrza@Aitmyrzas-MacBook-Pro.local>
2021-02-04 20:34:57 -0600

    Adding unity project

---
+++ Assets/EnablePlayerGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePlayerGravity : DefaultTrackableEventHandler
+{
+    public Rigidbody player;
+
+    override protected void OnTrackingLost()
+    {
+        player.useGravity = false;
+    }
+    override protected void OnTrackingFound()
+    {
+        player.useGravity = true;
+    }
+}
